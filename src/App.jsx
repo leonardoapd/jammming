1a6dbfe94b9d9343d470240bf4030957356c7142
Leonardo Perdomo <leonardop763@gmail.com>
2024-02-07 14:27:39 -0500

    Feat: SearchBar component and NES.css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+import { SearchBar } from './components';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<SearchBar />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import styles from './SearchBar.module.css';
+
+function SearchBar() {
+	const [userInput, setUserInput] = useState('');
+
+	const handleChange = (e) => {
+		const { value } = e.target;
+		setUserInput(value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(userInput);
+	};
+
+	return (
+		<>
+			<form onSubmit={handleSubmit} className={styles.app_form}>
+				<div className='nes-field'>
+					<label htmlFor='search' className={styles.app_label}>Search</label>
+					<input
+						type='text'
+						placeholder='Search...'
+						value={userInput}
+						onChange={handleChange}
+						className='nes-input'
+					/>
+				</div>
+				<button type='submit' className='nes-btn is-primary'>
+					Go
+				</button>
+			</form>
+		</>
+	);
+}
+
+export default SearchBar;
